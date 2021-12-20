5c1174fe4b5925072e07cd87830f600df44ede39
busylessman <weskero848@gmail.com>
2021-12-20 11:43:27 +0300

    All tasks done

--- main/Task3.java
+++ main/Task3.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Task3 {
-    public int sumOfArray(int[] nums) {
+    private int sumOfArray(int[] nums) {
         int sum = 0;
         for (int i: nums) {
             sum = sum + i;
@@ -12,7 +12,7 @@ public class Task3 {
         return sum;
     }
 
-    public int sumOfList(ArrayList<Integer> list) {
+    private int sumOfList(ArrayList<Integer> list) {
         int sum = 0;
         for (int i: list) {
             sum = sum + i;
@@ -20,7 +20,7 @@ public class Task3 {
         return sum;
     }
 
-    public int findBiggest(List<ArrayList<Integer>> list) {
+    private int findBiggest(List<ArrayList<Integer>> list) {
         int index = 0;
         for (int i = 1; i < list.size(); i++) {
             if (sumOfList(list.get(i)) > sumOfList(list.get(index))) {
@@ -30,7 +30,7 @@ public class Task3 {
         return index;
     }
 
-    public int findSmallest(List<ArrayList<Integer>> list) {
+    private int findSmallest(List<ArrayList<Integer>> list) {
         int index = 0;
         for (int i = 1; i < list.size(); i++) {
             if (sumOfList(list.get(i)) < sumOfList(list.get(index))) {
@@ -40,7 +40,7 @@ public class Task3 {
         return index;
     }
 
-    public List<ArrayList<Integer>> findBalanced(List<ArrayList<Integer>> list, int requiredSum) {
+    private List<ArrayList<Integer>> findBalanced(List<ArrayList<Integer>> list, int requiredSum) {
         List<ArrayList<Integer>> result = new ArrayList<>();
         for (ArrayList<Integer> a:list) {
             if (sumOfList(a) == requiredSum) {
@@ -51,7 +51,7 @@ public class Task3 {
         return result;
     }
 
-    public int findNearestNum(ArrayList<Integer> forBalance, int requiredSum) {
+    private int findNearestNum(ArrayList<Integer> forBalance, int requiredSum) {
         int excess = 0;
         for (int num : forBalance) {
             if (sumOfList(forBalance) - num >= requiredSum && num > excess)
@@ -60,7 +60,7 @@ public class Task3 {
         return excess;
     }
 
-    public void balance(List<ArrayList<Integer>> groups, int requiredSum) {
+    private void balance(List<ArrayList<Integer>> groups, int requiredSum) {
         ArrayList<Integer> biggest = groups.get(findBiggest(groups));
         ArrayList<Integer> smallest = groups.get(findSmallest(groups));
         int nearestNum = findNearestNum(biggest, requiredSum);
@@ -68,11 +68,11 @@ public class Task3 {
         smallest.add(nearestNum);
     }
 
-    public boolean isAbleToBalance ( int[] nums, int amount) {
+    private boolean isAbleToBalance ( int[] nums, int amount) {
         return !(sumOfArray(nums) % amount != 0 || nums.length < amount);
     }
 
-    public boolean stillAbleToBalance(List<ArrayList<Integer>> groups, int requiredSum) {
+    private boolean stillAbleToBalance(List<ArrayList<Integer>> groups, int requiredSum) {
         ArrayList<Integer> biggest = groups.get(findBiggest(groups));
         return findNearestNum(biggest, requiredSum) != 0;
     }
